d66ca4d1a164d8aea8a4a85b4759dc3d8f480553
Miguel Angel Femenia <mfemvaz2906@g.educaand.es>
2023-03-31 13:10:59 +0200

    Repository de Usuario

---
+++ src/main/java/com/mafv/academy/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.mafv.academy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mafv.academy.models.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+    
+    Usuario findByNombre(String username);
+}
