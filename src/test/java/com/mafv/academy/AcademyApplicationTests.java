097dd2e2f00f623edd5fad5f40e1595d17e1bc89
Miguel Angel Femenia <mfemvaz2906@g.educaand.es>
2023-03-31 12:16:27 +0200

    first commit

---
+++ src/test/java/com/mafv/academy/AcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mafv.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
