682c6df93d64b0b9da4113f348118668b8a95d8f
mafv <mfemvaz2906@g.educaand.es>
2023-04-17 08:20:30 +0200

    Controlador de Docente

--- src/main/java/com/mafv/academy/controller/DocenteController.java
+++ src/main/java/com/mafv/academy/controller/DocenteController.java
@@ -1,5 +1,91 @@
 package com.mafv.academy.controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.mafv.academy.models.Docente;
+import com.mafv.academy.services.DocenteService;
+
+@Controller
+@RequestMapping("/docentes")
 public class DocenteController {
     
+    @Autowired
+    DocenteService docentesService;
+    
+    @GetMapping(value = "/list")
+    public ModelAndView listPage(Model model) {
+
+        List<Docente> docentes = docentesService.findAll();
+
+        ModelAndView modelAndView = new ModelAndView("docentes/list");
+        modelAndView.addObject("docentes", docentes);
+
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/create")
+    public ModelAndView create(Docente docente) {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("docente", new Docente());
+        modelAndView.setViewName("docentes/create");
+
+        return modelAndView;
+    }
+
+    @PostMapping(path = "/save")
+    public ModelAndView save(Docente docente) throws IOException {
+
+        Docente cs = docentesService.save(docente);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:edit/" + cs.getCodigo());
+
+        return modelAndView;
+    }
+
+    @GetMapping(path = { "/edit/{id}" })
+    public ModelAndView edit(
+            @PathVariable(name = "id", required = true) int id) {
+
+        Docente docente = docentesService.findById(id);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("docente", docente);
+        modelAndView.setViewName("docentes/edit");
+        return modelAndView;
+    }
+
+    @PostMapping(path = { "/update" })
+    public ModelAndView update(Docente docente) {
+
+        docentesService.update(docente);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:edit/" + docente.getCodigo());
+
+        return modelAndView;
+    }
+
+    @GetMapping(path = { "/delete/{id}" })
+    public ModelAndView delete(
+            @PathVariable(name = "id", required = true) int id) {
+
+        docentesService.deleteById(id);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/docentes/list");
+
+        return modelAndView;
+    }
 }
