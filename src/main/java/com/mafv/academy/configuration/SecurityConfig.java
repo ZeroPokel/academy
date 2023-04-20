f4ac4f8a42178effed4770c420058635aeeeef05
mafv <mfemvaz2906@g.educaand.es>
2023-04-20 13:30:40 +0200

    Update

--- src/main/java/com/mafv/academy/configuration/SecurityConfig.java
+++ src/main/java/com/mafv/academy/configuration/SecurityConfig.java
@@ -3,8 +3,6 @@ package com.mafv.academy.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
