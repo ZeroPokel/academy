236060b8bfe3d7a607ba0874017562263bbcaee1
Miguel Angel <mfemvaz2906@g.educaand.es>
2023-04-24 08:19:37 +0200

    Login custom incompleto, falta logout y plantilla

--- src/main/java/com/mafv/academy/configuration/SecurityConfig.java
+++ src/main/java/com/mafv/academy/configuration/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,7 +18,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import com.mafv.academy.services.UsuariosService;
 
 @Configuration
-public class SecurityConfig{
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
     
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -56,18 +60,23 @@ public class SecurityConfig{
         return autProvider;
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        
-        http
-            .authorizeRequests()
-            .anyRequest()
-            .authenticated()
-        .and()
-            .formLogin()
-        .and()
-            .httpBasic();
-        
-        return http.build();
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**");
+        web.ignoring().antMatchers("/js/**");
     }
 }
--- src/main/java/com/mafv/academy/controller/LoginController.java
+++ src/main/java/com/mafv/academy/controller/LoginController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-    
-    @RequestMapping(value = "/login")
-    public String login(){
-        return "login";
-    }
 
     @GetMapping(value = {"/welcome"})
     public String welcome(){
