d1d6c5b8d7fde2117f216679accc19c07cdd18f9
Miguel Angel <mfemvaz2906@g.educaand.es>
2023-04-26 14:41:47 +0200

    Limpieza de imports

--- src/main/java/com/mafv/academy/controller/CursoController.java
+++ src/main/java/com/mafv/academy/controller/CursoController.java
@@ -3,7 +3,6 @@ package com.mafv.academy.controller;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mafv.academy.models.Curso;
