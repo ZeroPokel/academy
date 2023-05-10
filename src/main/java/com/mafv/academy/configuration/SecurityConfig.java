27f7197542c7c64f7a63e8a6bfd3f93ad705bf44
Miguel Angel <mfemvaz2906@g.educaand.es>
2023-05-10 14:57:21 +0200

    Limpiado y arreglado codigo deprecated

--- src/main/java/com/mafv/academy/configuration/SecurityConfig.java
+++ src/main/java/com/mafv/academy/configuration/SecurityConfig.java
@@ -2,31 +2,26 @@ package com.mafv.academy.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import com.mafv.academy.services.UsuariosService;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig{
     
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         String[] staticResources  =  {
             "/css/**",
@@ -52,12 +47,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                     .invalidateHttpSession(true)
                     .deleteCookies("JSESSIONID")
                 .permitAll();
+
+        return http.build();
     }
 
     // Evita que te redirija a cualquier css o js
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**");
-        web.ignoring().antMatchers("/js/**");
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
+        return (web) -> web.ignoring().antMatchers("/css/**", "/js/**");
     }
 }
