f89da731e3185d46d65f7477fbbecce240a2a118
Miguel Angel Femenia <mfemvaz2906@g.educaand.es>
2023-05-12 10:07:07 +0200

    Limpieza de imports

--- src/main/java/com/mafv/academy/controller/LoginController.java
+++ src/main/java/com/mafv/academy/controller/LoginController.java
@@ -10,11 +10,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mafv.academy.models.Usuario;
 import com.mafv.academy.models.UsuarioDTO;
