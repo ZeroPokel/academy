528ba1b732c01d2cb70edef3671594b09458c750
Miguel Angel Femenia Vazquez <miguelangel2000pokel@gmail.com>
2023-06-20 14:40:19 +0200

    Añadido sql de la BBDD entera

--- src/main/resources/static/sql/Akademy.sql
+++ src/main/resources/static/sql/Akademy.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: academy
+-- Host: localhost    Database: academy
 -- ------------------------------------------------------
 -- Server version	8.0.32
 
@@ -16,6 +16,23 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `curso`
+--
+
+DROP TABLE IF EXISTS `curso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `curso` (
+  `codigo` int NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `tutor` int DEFAULT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `FKo9q1qe24yu6h7f28k4fvh3xmb` (`tutor`),
+  CONSTRAINT `FKo9q1qe24yu6h7f28k4fvh3xmb` FOREIGN KEY (`tutor`) REFERENCES `usuario` (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `curso`
 --
 
@@ -24,6 +41,26 @@ LOCK TABLES `curso` WRITE;
 INSERT INTO `curso` VALUES (5,'Desarrollo de Aplicaciones Web',4),(6,'Desarrollo de Aplicaciones Multiplataforma',NULL),(7,'Administración de Sistemas Informáticos en Red',27),(8,'1º Bachillerato',NULL),(9,'2º Bachillerato',NULL),(10,'4º ESO',NULL),(11,'3º ESO',NULL);
 /*!40000 ALTER TABLE `curso` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `estudiantemodulo`
+--
+
+DROP TABLE IF EXISTS `estudiantemodulo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estudiantemodulo` (
+  `estudiante_codigo` int NOT NULL,
+  `modulo_codigo` int NOT NULL,
+  `primEv` float NOT NULL,
+  `segunEv` float NOT NULL,
+  `tercEv` float NOT NULL,
+  PRIMARY KEY (`estudiante_codigo`,`modulo_codigo`),
+  KEY `FK716fmwx6cdbxhlhlegooka11f` (`modulo_codigo`),
+  CONSTRAINT `FK716fmwx6cdbxhlhlegooka11f` FOREIGN KEY (`modulo_codigo`) REFERENCES `modulo` (`codigo`),
+  CONSTRAINT `FKs4bbwropwabxt8llvc7ajkhj1` FOREIGN KEY (`estudiante_codigo`) REFERENCES `usuario` (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `estudiantemodulo`
@@ -36,14 +73,46 @@ INSERT INTO `estudiantemodulo` VALUES (2,12,3,7,5),(2,13,0,0,0),(2,16,0,0,0),(24
 UNLOCK TABLES;
 
 --
+-- Table structure for table `hibernate_sequence`
+--
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `hibernate_sequence`
 --
 
 LOCK TABLES `hibernate_sequence` WRITE;
 /*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
-INSERT INTO `hibernate_sequence` VALUES (29);
+INSERT INTO `hibernate_sequence` VALUES (1),(29);
 /*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `modulo`
+--
+
+DROP TABLE IF EXISTS `modulo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `modulo` (
+  `codigo` int NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `curso` int DEFAULT NULL,
+  `docente` int DEFAULT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `FKakhfh3bj3esc9qqw1ryvruqug` (`curso`),
+  KEY `FKp2swh73w9slla92foc5t12iax` (`docente`),
+  CONSTRAINT `FKakhfh3bj3esc9qqw1ryvruqug` FOREIGN KEY (`curso`) REFERENCES `curso` (`codigo`),
+  CONSTRAINT `FKp2swh73w9slla92foc5t12iax` FOREIGN KEY (`docente`) REFERENCES `usuario` (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `modulo`
@@ -56,6 +125,20 @@ INSERT INTO `modulo` VALUES (12,'Programacion',5,27),(13,'Sistemas Informaticos'
 UNLOCK TABLES;
 
 --
+-- Table structure for table `permiso`
+--
+
+DROP TABLE IF EXISTS `permiso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permiso` (
+  `codigo` varchar(255) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `permiso`
 --
 
@@ -66,6 +149,23 @@ INSERT INTO `permiso` VALUES ('ADMIN','ADMIN'),('DOCENTE','DOCENTE'),('ESTUDIANT
 UNLOCK TABLES;
 
 --
+-- Table structure for table `user_permission`
+--
+
+DROP TABLE IF EXISTS `user_permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_permission` (
+  `Usuario_codigo` int NOT NULL,
+  `Permiso_codigo` varchar(255) NOT NULL,
+  KEY `FKlvwpvmgqi7w7v9vdcb4o8ns35` (`Permiso_codigo`),
+  KEY `FKoqamr50nakyqgfak5qi1oaxm2` (`Usuario_codigo`),
+  CONSTRAINT `FKlvwpvmgqi7w7v9vdcb4o8ns35` FOREIGN KEY (`Permiso_codigo`) REFERENCES `permiso` (`codigo`),
+  CONSTRAINT `FKoqamr50nakyqgfak5qi1oaxm2` FOREIGN KEY (`Usuario_codigo`) REFERENCES `usuario` (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `user_permission`
 --
 
@@ -74,6 +174,31 @@ LOCK TABLES `user_permission` WRITE;
 INSERT INTO `user_permission` VALUES (1,'ADMIN'),(2,'ESTUDIANTE'),(24,'ESTUDIANTE'),(25,'ESTUDIANTE'),(26,'ESTUDIANTE'),(28,'DOCENTE'),(4,'DOCENTE'),(4,'TUTOR'),(27,'DOCENTE'),(27,'TUTOR');
 /*!40000 ALTER TABLE `user_permission` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `tipo_usuario` varchar(31) NOT NULL,
+  `codigo` int NOT NULL,
+  `apellidos` varchar(255) DEFAULT NULL,
+  `dni` varchar(9) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `fechaNacimiento` datetime(6) DEFAULT NULL,
+  `foto` longblob,
+  `nombre` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `sexo` varchar(255) DEFAULT NULL,
+  `telefono` bigint DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`codigo`),
+  UNIQUE KEY `UK_7j3a9k87df81ualfh3cym0u5l` (`dni`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `usuario`
@@ -102,4 +227,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-19 16:08:51
+-- Dump completed on 2023-06-20 14:36:21
